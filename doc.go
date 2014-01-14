7195ef4d561732cb058a3b6502a9ad159441c4ef
Rafael Garcia <rafael.garcia@clever.com>
2014-01-14 10:47:18 -0800

    docs: add usage

--- doc.go
+++ doc.go
@@ -1,3 +1,26 @@
 // Package gearadmin provides simple bindings to the gearman admin protocol: http://gearman.org/protocol/.
-
+//
+//
+// Usage
+//
+// Here's an example program that tails all operations starting from the time the program is launched:
+//
+//         package main
+//
+//         import (
+//         	"fmt"
+//         	"github.com/Clever/gearadmin"
+//         	"net"
+//         )
+//
+//         func main() {
+//         	c, err := net.Dial("tcp", "localhost:4730")
+//         	if err != nil {
+//         		panic(err)
+//         	}
+//         	defer c.Close()
+//         	gearadmin := gearadmin.NewGearmanAdmin(c)
+//         	status, _ := gearadmin.Status()
+//         	fmt.Printf("%#v\n", status)
+//         }
 package gearadmin
